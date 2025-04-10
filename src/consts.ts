5efb7815149762181fea5087f1c1b7a703347c7a
Dillion Megida <dillionmegida@gmail.com>
2025-04-10 20:59:52 +0200

    setup astro for project

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'FurnZero - Zero Furniture Waste';
+export const SITE_DESCRIPTION = 'FurnZero - Zero Furniture Waste';
